55fc02ab8d96507a85ca7b77da8cc636f8612833
vino121095 <168708428+vino121095@users.noreply.github.com>
2025-10-31 12:51:39 +0530

    Api changes

--- src/components/ApiService/ApiService.js
+++ src/components/ApiService/ApiService.js
@@ -1,3 +1,4 @@
-// const baseurl = "https://velanmartbackend.deecodes.io"
-const baseurl = "http://localhost:5000";
+const baseurl = "https://velanmartbackend.deecodes.io"
+// const baseurl = "http://localhost:5000";
 export default baseurl;
+
